aa138a1d2b9459d789865f93a78849ca5bc64899
Jesus Romero <69400254+liam075@users.noreply.github.com>
2024-05-10 14:33:58 -0400

    Primer Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutComponent } from './Pages/about/about.component';
+export const routes: Routes = [
+  {path: '',
+  redirectTo: 'home',
+  pathMatch: 'full'},
 
-export const routes: Routes = [];
+  { path: 'home', component:HomeComponent },
+  { path: 'about', component: AboutComponent}
+];
